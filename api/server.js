1c094efdd047bb1d23a709d216f61fce09ae0629
kartikeya <kartikeyaverma08@gmail.com>
2026-02-14 23:58:27 +0530

    Add MongoDB container and notes API

--- api/server.js
+++ api/server.js
@@ -1,10 +1,33 @@
-const express = require('express');
-const app = express();
+const express = require("express");
+const mongoose = require("mongoose");
 
-app.get('/', (req, res) => {
-  res.send('DevOps Notes API Running');
+const app = express();
+app.use(express.json());   // <--- needed for POST body
+
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+// Note model
+const Note = mongoose.model("Note", {
+  title: String,
+  content: String
 });
 
-app.listen(3000, "0.0.0.0", () => {
-  console.log("Server running on port 3000");
+// Test route
+app.get("/", (req, res) => {
+  res.send("DevOps Notes API Running");
+});
+
+// Create note route
+app.post("/notes", async (req, res) => {
+  const note = await Note.create(req.body);
+  res.json(note);
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
